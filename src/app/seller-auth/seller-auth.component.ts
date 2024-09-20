27b1be7a5cb89144ec93e3ed646bc848a94ba6c9
Kushal Kumar <kumar.kush111@gmail.com>
2024-09-20 17:53:32 +0530

    initial commit

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-seller-auth',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './seller-auth.component.html',
+  styleUrl: './seller-auth.component.css'
+})
+export class SellerAuthComponent {
+  constructor() {
+
+  }
+  signUp(data: object) {
+    console.log(data);
+  }
+}
